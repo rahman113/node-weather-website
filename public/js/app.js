d546d9cb9da281483c0197849c5bfe7ffd376f5a
Ataurrahman <ataurrahman9731@gmail.com>
2022-01-23 01:09:47 +0530

    remove unnecessary console log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log("Clinet side java script file is loaded!");
+
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
