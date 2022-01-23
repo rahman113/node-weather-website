1b5ba5eb2d723787f8309341148a99e086239639
Ataurrahman <ataurrahman9731@gmail.com>
2022-01-23 15:43:51 +0530

    add localtime and timezone in utils/forecat

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -17,9 +17,6 @@ const geocode = (address, callback) =>{
                  latitude:  response.body.features[0].center[1],
                  longitude:  response.body.features[0].center[0],
                  location : response.body.features[0].place_name,
-                
-                
-            
                 })
             }
         })
