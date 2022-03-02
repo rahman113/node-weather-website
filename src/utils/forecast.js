23168b01451882f0e25c945b2603a9bf19a5e80d
Ataurrahman <ataurrahman9731@gmail.com>
2022-03-02 17:15:18 +0530

    some modification in forecating

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) =>{
         } else if (response.body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out." + 'this is timezone id ' + response.body.location.timezone_id + 'this is localtime of a specified location ' + response.body.location.localtime)
+            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out." + 'this is timezone id ' + response.body.location.timezone_id + ' this is localtime of a specified location ' + response.body.location.localtime)
         }
     })
 
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,3 +1,4 @@
+
 const request = require('request');
 
 const geocode = (address, callback) =>{
